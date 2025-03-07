2cf5a89fff7801c2952babd484c90fda82d2555f
GAUTAMVERM <89487056+GAUTAMVERM@users.noreply.github.com>
2025-03-07 15:21:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstWord(s) {
   // your code here
+	if(s.length===0){
+		return s;
+	}
+	let fis = s.split(" ")[0];
+	return fis;
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
+
+
